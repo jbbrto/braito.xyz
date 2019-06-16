9e3962fca295d356989d01aa56b17f5845e9484f
Jakob Braito <>
2019-06-16 23:28:21 +0200

    ad dummy alpha

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
 
-    alert("jQuery active");
+    // console.log("jQuery active");
 
-    $("img-ad").mouseover(function() {
-        $("img-ad").attr('src','img/test.png');
-    });
+    // Query if mobile device
+    if ($(window).width()<768){
 
-    $("img-ad").mouseout(function() {
-        $("img-ad").attr('src','img/ad_AX_plain.jpg');
-    });
+        // Change text for mobile touch devices
+        $("#item-text").html("<b>Touch and hold the 'Google Dummy Ad' in order to reveal your very own personalized ad.</b>");
+
+        // Mobile touch-hold
+        $("#ad").bind('touchstart', function(){
+            $("#ad").attr('src', 'img/redbull_Large_skyscraper_300x600.png');
+            console.log("touchstart");
+        });
+        $("#ad").bind('touchend', function(){
+            $("#ad").attr('src', 'img/ad_Large_skyscraper_300x600.png');
+            console.log("touchend");
+        });
+
+
+    } else {
+
+        <!-- jQuery Library einlesen -->
+        src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+
+        $("#ad").mouseover(function () {
+            $("#ad").attr('src', 'img/redbull_Large_skyscraper_300x600.png');
+        });
+
+        $("#ad").mouseout(function () {
+            $("#ad").attr('src', 'img/ad_Large_skyscraper_300x600.png');
+        });
+
+    }
+
+
+
+
 
 });
 
